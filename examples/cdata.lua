32f19228835f59c9898205515267117d0ea467cb
megagrump <megagrump@protonmail.com>
2021-02-13 23:58:54 +0100

    expand cdata example

--- examples/cdata.lua
+++ examples/cdata.lua
@@ -3,24 +3,48 @@ package.path = package.path .. ';../?.lua'
 local BlobReader, BlobWriter = require('BlobReader'), require('BlobWriter')
 local ffi = require('ffi')
 
-ffi.cdef('typedef struct { double x, y; } teststruct_t;')
+ffi.cdef([[
+	typedef struct {
+		double x, y;
+		const char *name;
+	} teststruct_t;
+]])
 
+-- LuaJIT's ffi API does not provide a way to retrieve the declared name of ctypes.
+-- To enable transparent serialization of cdata (e.g. cdata in tables), additional metadata has to be added
+-- to the metatable of the ctype.
 local metatype = {
 	__index = {
-		__typename = 'teststruct_t', -- the typename from the cdef declaration
+		-- the typename from the cdef declaration
+		-- if this identifier is present, the ctype can be automatically serialized
+		-- if not present, only BlobWriter:cdata() and BlobReader:cdata() can be used with this type
+		__typename = 'teststruct_t',
 
-		__serialize = function(self, writer) -- called when writing data
-			return writer:number(self.x):number(self.y)
+		-- custom serialization of cdata. called when a cdata object is serialized
+		-- this function is only required for ctypes that contain data of unknown size (i.e. pointers)
+		__serialize = function(self, writer)
+			return writer
+				:number(self.x)
+				:number(self.y)
+				:string(ffi.string(self.name))
 		end,
 
-		__deserialize = function(ctype, reader) -- called when reading data
-			return ctype(reader:number(), reader:number())
+		-- custom deserialization of cdata. called when a cdata object is deserialized
+		-- this function is only required for ctypes that have a __serialize function
+		__deserialize = function(ctype, reader)
+			return ctype(
+				reader:number(),
+				reader:number(),
+				reader:string()
+			)
 		end,
 	}
 }
 
-local ctype = ffi.metatype(ffi.typeof('teststruct_t'), metatype) -- add serialization functions to teststruct_t
-local cdata = ctype(23, 42)
+-- add serialization information to teststruct_t
+local ctype = ffi.metatype(ffi.typeof('teststruct_t'), metatype)
+
+local cdata = ctype(23, 42, 'test')
 
 -- write cdata
 local serialized = BlobWriter():write(cdata):tostring()
@@ -28,6 +52,8 @@ local serialized = BlobWriter():write(cdata):tostring()
 -- read cdata
 local deserialized = BlobReader(serialized):read()
 
+-- cdata and deserialized should be identical
 print("Deserialized object is of type " .. deserialized.__typename)
-print("Input values: ", cdata.x, cdata.y)
-print("Output values: ", deserialized.x, deserialized.y)
+print("Input values: ", cdata.x, cdata.y, ffi.string(cdata.name))
+print("Output values: ", deserialized.x, deserialized.y, ffi.string(deserialized.name))
+
